6da21c2263bc434890fd35d12c92d1d419b06a80
Michael Nagler <m.nagler995@gmail.com>
2018-09-17 18:26:59 -0400

    finished app by following along with the video lectures

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
+import AudioToolbox
 
 class ViewController: UIViewController{
+    
+    
+    var player = AVAudioPlayer() 
+    
+    var noteResourceNames = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
 
     override func viewDidLoad() {
@@ -18,9 +25,43 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        playSound(noteResource: noteResourceNames[sender.tag - 1]) 
+        
+        // This is a way to do it using AVFoundation
+//        // sender has a property called "tag" which is an attribute and can be set under "View" in attribute inspector, i.e. sender.tag
+//        let url = Bundle.main.url(forResource: noteResourceName, withExtension: "wav")!
+//
+//        do {
+//            player = try AVAudioPlayer(contentsOf: url)
+//            guard let player = player else { return }
+//            player.prepareToPlay()
+//            player.play()
+//        } catch let error as Error {
+//            print(error.localizedDescription)
+//        }
+        // And here's a way using AudioToolBox
+//        if let url = Bundle.main.url(forResource: noteResourceName, withExtension: "wav") {
+//            var mySound: SystemSoundID = 0
+//            AudioServicesCreateSystemSoundID(url as CFURL, &mySound)
+//            //play
+//            AudioServicesPlaySystemSound(mySound)
+//        }
         
         
         
+    }
+    
+    func playSound(noteResource: String) {
+        // And here's the way recommended by the lesson. Like way 1 bt a bit simpler (no guards, etc.)
+        
+        let url = Bundle.main.url(forResource: noteResource, withExtension: "wav")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: url!)
+        } catch {
+            print(error)
+        }
+        player.play()
     }
     
   
